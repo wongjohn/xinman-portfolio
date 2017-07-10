38c77ebe36b490299690671b448bafaa64f9552f
wangshijiang <wangshijiang@eqxiu.com>
2017-07-10 12:28:08 +0800

    修改右侧菜单标题为网站名称

--- header.php
+++ header.php
@@ -23,10 +23,8 @@
 	</head>
 	<body <?php body_class(); ?> >
 		<div id="Menu" style="top:80px; left:900px; position:fixed; text-align:center;">
-			<span class="Name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" style="color:#000;">单馨漫</a></span>
+			<span class="Name"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" style="color:#000;"><?php bloginfo( 'name' ); ?></a></span>
 			<div style="margin-top:40px; text-transform:uppercase; font-size:11px; letter-spacing:1px;">
-				<div><span style="padding-right:30px; color:#ccc;">摄影作品</span></div>
-
 				<div style="text-transform:none; font-size:12px; font-family:tahoma; text-align:left; margin:10px 0px; padding-left:10px;">
 					<div><a href="<?php echo esc_url( home_url( '/' ) ); ?>">Portraits</a></div>
 					<div><a href="<?php echo esc_url( home_url( '/' ) ); ?>">New York</a></div>
