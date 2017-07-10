5e965bb670fa601cf6b1284f310f85b4c116c48a
wangshijiang <wangshijiang@eqxiu.com>
2017-07-10 08:39:40 +0800

    Add post-thumbnails support.

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+/*
+# =================================================
+# function.php
+#
+# The theme's functions.
+# =================================================
+*/
+/* Add support for post thumbnails. */
+add_theme_support( 'post-thumbnails' );
+
+//set_post_thumbnail_size( 50, 50 );
