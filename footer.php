32aebf8d62cd3f70f2db2a5856de1737f42fc233
wangshijiang <wangshijiang@eqxiu.com>
2017-07-16 18:47:24 +0800

    添加代码，将所有图片，都置为可以"弹出放大"

--- footer.php
+++ footer.php
@@ -10,6 +10,7 @@
 
 		<?php wp_footer(); ?>
 		<script src="<?php echo get_template_directory_uri();?>/js/jquery-1.10.2.min.js" type="text/javascript"></script>
+		<script src="<?php echo get_template_directory_uri();?>/js/main.js" type="text/javascript"></script>
 		<script src="<?php echo get_template_directory_uri();?>/js/lightbox-2.6.min.js" type="text/javascript"></script>
 	</body>
 </html>
